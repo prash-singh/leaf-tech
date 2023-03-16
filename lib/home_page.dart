86bfa1117715b2fa9098b4851627b437811a5007
Prashant Singh <prashantsingh2k2@gmail.com>
2023-03-16 18:39:51 +0530

    Some changes and addon in UI and connected to firebase

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: use_build_context_synchronously
-
+import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:leaf_tech/details.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +14,38 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   File? _image;
+  //final String _docType = "Rice Brown Spot";
+  late String _docType;
+  late List precaution, pesticides;
+  bool loader = true;
+  late var info = "";
+
+  _fetchData() async {
+    Random random = Random();
+    int num = random.nextInt(2);
+    (num == 0) ? _docType = "Leaf Smut" : _docType = "Rice Brown Spot";
+    var tempInfo = "";
+    List tempPrecaution = [], tempPesticides = [];
+    var db = await FirebaseFirestore.instance
+        .collection("Rice Leaf")
+        .doc(_docType)
+        .get();
+    if (db.exists) {
+      Map<String, dynamic>? data = db.data();
+      tempInfo = data?['Information'];
+      tempPrecaution = data?['Precaution'];
+      tempPesticides = data?['Pesticides'];
+    }
+
+    setState(() {
+      info = tempInfo;
+      precaution = tempPrecaution;
+      pesticides = tempPesticides;
+      loader = false;
+      _showResult(context);
+    });
+  }
+
   _openGallary() async {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
     final imageTemp = File(image!.path);
@@ -31,6 +64,47 @@ class _HomePageState extends State<HomePage> {
     Navigator.of(context).pop();
   }
 
+  Future<void> _showResult(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Details(
+                              info: info,
+                              precaution: precaution,
+                              pesticides: pesticides,
+                            )));
+                  },
+                  child: const Text("More Detail")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("close"))
+            ],
+            actionsAlignment: MainAxisAlignment.spaceBetween,
+            title: const Text("Result"),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _decideImageView(),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Text('Type: $_docType',style: const TextStyle(fontSize: 18, fontStyle: FontStyle.italic, color: Colors.deepOrange),),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   Future<void> _showChoiceDialog(BuildContext context) {
     return showDialog(
       context: context,
@@ -39,24 +113,24 @@ class _HomePageState extends State<HomePage> {
           title: const Text("Make a choice"),
           content: SingleChildScrollView(
               child: ListBody(
-            children: [
-              GestureDetector(
-                child: const Text("Gallery"),
-                onTap: () {
-                  _openGallary();
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-              ),
-              GestureDetector(
-                child: const Text("Camera"),
-                onTap: () {
-                  _openCamera();
-                },
-              ),
-            ],
-          )),
+                children: [
+                  GestureDetector(
+                    child: const Text("Gallery"),
+                    onTap: () {
+                      _openGallary();
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: const Text("Camera"),
+                    onTap: () {
+                      _openCamera();
+                    },
+                  ),
+                ],
+              )),
         );
       },
     );
@@ -80,7 +154,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.green,
         leading: const Icon(Icons.energy_savings_leaf),
-        title: const Text("Leaf Disease Info"),
+        title: const Text("Leaf Tech"),
         actions: const [
           Padding(
             padding: EdgeInsets.all(16.0),
@@ -92,14 +166,34 @@ class _HomePageState extends State<HomePage> {
         margin: const EdgeInsets.symmetric(vertical: 12.0),
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               _decideImageView(),
-              ElevatedButton(
+              const SizedBox(height: 16.0,),
+              (_image == null)
+                  ? ElevatedButton(
                 onPressed: () {
                   _showChoiceDialog(context);
                 },
                 child: const Text("Select Image"),
+              )
+                  : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _fetchData();
+                    },
+                    child: const Text("Generate Result"),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _image = null;
+                        });
+                      },
+                      child: const Text("Remove Image"))
+                ],
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
